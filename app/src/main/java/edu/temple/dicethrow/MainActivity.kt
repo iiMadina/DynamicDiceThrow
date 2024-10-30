1d5dddbae94fe208c871c17817239ad2bee3a18d
iiMadina <misrailova11@gmail.com>
2024-10-30 16:24:30 -0400

    finished TODOS

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,6 +1,9 @@
 package edu.temple.dicethrow
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,17 +26,31 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* TODO 1: Load fragment(s)
-            - Show only Button Fragment if portrait
-            - show both fragments if Landscape
-          */
+        if (findViewById<View>(R.id.container1) != null) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1, ButtonFragment())
+                    .add(R.id.container2, DieFragment())
+                    .commit()
+            } else {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1, ButtonFragment())
+                    .commit()
+            }
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
         */
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
-
+        if (findViewById<View>(R.id.container2) == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container1, DieFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
 
